49ebb7580f60cf4100c838f07b2fe7a50b7a050c
Chan-Seok Jeong <csjeong@kaist.ac.kr>
2016-02-19 16:42:24 +0900

    feat: modify command-line option
    The old command-line arguments are changed as below
    - Arguments <msa_file> and <pdb_file> are required
    - Option --pos specify the output file for positional coevolution scores
    - Option --pair specify the output file for pairwise coevolution scores
    
    New options are added to write MRF modeling information
    - Option --edge specify the edge file representing MRF architecture
    - Option --mrf specify the MRF file for SMRF model
    
    Option -h, --help is added to show help message
    Option --version is added to show the release version

--- smrf.py
+++ smrf.py
@@ -7,12 +7,13 @@
 #
 # If you use this software for any published work, please cite the accompanying paper.
 #
-#   Chan-Seok Jeong and Donsup Kim (2016) Structure-based Markov random field model for representing evolutionary constraints on functional sites. submitted.
+#   Chan-Seok Jeong, and Donsup Kim (2016) Structure-based Markov random field model for representing evolutionary constraints on functional sites. BMC Bioinformatics. Accepted.
 #
 
 import sys
 import tempfile
 import os.path
+import optparse
 import ConfigParser
 
 from edge import build_edge
@@ -38,11 +39,8 @@ def load_config():
     cfg.read(CONFIG_FILE)
     return {'pmrf_path': os.path.expanduser(cfg.get('external', 'pmrf_path').split(';')[0])}
 
-def smrf(afa_file, pdb_file, out_file1, out_file2, pmrf_path):
-    edge_tf = tempfile.NamedTemporaryFile()
-    edge_file = edge_tf.name
-    mrf_tf = tempfile.NamedTemporaryFile()
-    mrf_file = mrf_tf.name
+def smrf(afa_file, pdb_file, options, pmrf_path):
+    edge_file, mrf_file = options.edge_file, options.mrf_file
 
     edge_list = build_edge(afa_file, pdb_file)
     write_file('\n'.join(['%s\t%s'%(i, j) for i, j in edge_list]), edge_file)
@@ -52,35 +50,37 @@ def smrf(afa_file, pdb_file, out_file1, out_file2, pmrf_path):
     message('MRF model is parameterized.')
 
     pos_score = calc_pos_score(mrf_file, afa_file)
-    write_file('\n'.join(['%d\t%f'%(idx, val) for idx, val in pos_score]), out_file1)
-    message('Positional coevolution scores are written in %s.'%(out_file1))
+    write_file('\n'.join(['%d\t%f'%(idx, val) for idx, val in pos_score]), options.score_file1)
+    message('Positional coevolution scores are written in %s.'%(options.score_file1))
 
-    pair_score = calc_pair_score(mrf_file)
-    write_file('\n'.join(['%d\t%d\t%f'%(x.i, x.j, x.score) for x in pair_score]), out_file2)
-    message('Pairwise coevolution scores are written in %s.'%(out_file2))
+    if options.score_file2:
+        pair_score = calc_pair_score(mrf_file)
+        write_file('\n'.join(['%d\t%d\t%f'%(x.i, x.j, x.score) for x in pair_score]), options.score_file2)
+        message('Pairwise coevolution scores are written in %s.'%(options.score_file2))
 
-def print_help():
-    print """\
-Usage: %s [msa_file] [pdb_file] [out_file1] [out_file2]
-
-Input:
-  msa_file      Multiple sequence alignment (FASTA format)
-  pdb_file      Protein structure (PDB format)
-
-Output:
-  out_file1     Positional coevolution score
-                Column #1: residue position
-                Column #2: score
-  out_file2     Pairwise coevolution score
-                Column #1: residue position #1
-                Column #2: residue position #2
-                Column #3: score
-"""%sys.argv[0]
+def parse_options():
+    parser = optparse.OptionParser(usage = "%prog <msa_file> <pdb_file> [options]",
+                                   version="%prog " + VERSION)
+    grp = optparse.OptionGroup(parser, "Output options")
+    grp.add_option("--pos", dest="score_file1", default="stdout", help="write positional coevolution scores to <score_file> (default: stdout)", metavar="<score_file>")
+    grp.add_option("--pair", dest="score_file2", default=None, help="write pairwise coevolution scores to <score_file>", metavar="<score_file>")
+    grp.add_option("--edge", dest="edge_file", default=None, help="write MRF edge list to <edge_file>", metavar="<edge_file>")
+    grp.add_option("--mrf", dest="mrf_file", default=None, help="write MRF model to <mrf_file>", metavar="<mrf_file>")
+    parser.add_option_group(grp)
+    (options, args) = parser.parse_args()
+    if len(args) != 2: parser.error("Incorrect number of arguments. See '%s --help'."%os.path.basename(sys.argv[0]))
+    for arg in args:
+        if not os.path.exists(arg): parser.error("The file %s does not exist"%arg)
+    (msa_file, pdb_file) = args
+    return msa_file, pdb_file, options
 
 if __name__ == '__main__':
     cfg = load_config()
-    if len(sys.argv[1:]) != 4:
-        print_help()
-        sys.exit(1)
-    afa_file, pdb_file, out_file1, out_file2 = sys.argv[1:]
-    smrf(afa_file, pdb_file, out_file1, out_file2, cfg['pmrf_path'])
+    afa_file, pdb_file, options = parse_options()
+    if not options.edge_file:
+        edge_tf = tempfile.NamedTemporaryFile()
+        options.edge_file = edge_tf.name
+    if not options.mrf_file:
+        mrf_tf = tempfile.NamedTemporaryFile()
+        options.mrf_file = mrf_tf.name
+    smrf(afa_file, pdb_file, options, cfg['pmrf_path'])
